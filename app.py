3f49eac7e6d79919e12e1525af655dd7f71f7f8c
harshalsky24 <harshalsky24@gmail.com>
2025-09-18 10:12:12 +0530

    Add To-Do Page frontend with form (Item Name, Item Description)

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import json, os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/api', methods=['GET'])
+def get_data():
+    file_path = os.path.join(os.path.dirname(__file__), 'data.json')
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return jsonify(data)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
